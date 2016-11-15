c0f312fcfd7059cb7dcf4ae88960e8c0edaf8dc3
dan <dan@702010forum.com>
2016-11-15 15:33:45 +1100

    Up to Video 13 - State

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -1,16 +1,35 @@
 import React from 'react';
+import { getFunName } from '../helpers';
 
 class StorePicker extends React.Component {
+
+    constructor() {
+        super();
+        this.goToStore = this.goToStore.bind(this);
+    }
+
+    goToStore(event) {
+        event.preventDefault();
+        console.log("changed url");
+        //get value
+        const storeId = this.storeInput.value;
+        this.context.router.transitionTo(`/store/${storeId}`);
+
+    }
+
     render() {
         return (
-           <form className="store-selector">
+           <form className="store-selector" onSubmit={this.goToStore}>
                <h2>Please Enter a Store</h2>
-               <input type="text" required placeholder="Store Name" />
+               <input type="text" required placeholder="Store Name" defaultValue={getFunName()} ref={(input) => {this.storeInput = input}} />
                <button type="submit">Visit Store ></button>
            </form>
-
         )
     }
+}
+
+StorePicker.contextTypes = {
+    router: React.PropTypes.object
 }
 
 export default StorePicker;
--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,8 +1,23 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { BrowserRouter, Match, Miss } from 'react-router';
+
 import './css/style.css';
+
 import StorePicker from './components/StorePicker';
 import App from './components/App';
+import NotFound from './components/NotFound';
 
+const Root = () => {
+    return (
+        <BrowserRouter>
+            <div>
+                <Match exactly pattern="/" component={StorePicker} />
+                <Match exactly pattern="/store/:storeId" component={App} />
+                <Miss component={NotFound} />
+            </div>
+        </BrowserRouter>
+    )
+};
 
-render(<App/>, document.querySelector('#main'));
+render(<Root/>, document.querySelector('#main'));
